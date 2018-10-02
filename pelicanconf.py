7bf4993c514d3ee549510672d96987ebfa687c02
Andrei Halanay <halanay@fmi.unibuc.ro>
2018-10-02 23:38:44 +0300

    Python

---
+++ pelicanconf.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Andrei Halanay'
+SITENAME = u'My Web Site'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Bucharest'
+DEFAULT_DATE = 'fs'
+STATIC_PATHS = ['pdf']
+THEME = "/home/andrei/repozitorii/elegant"
+#PAGINATED_DIRECT_TEMPLATES = ('categories', 'archives')
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+PLUGIN_PATHS = ['../pelican-plugins']
+PLUGINS = ['render_math', 'gravatar']
+TYPOGRIFY = True
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 30
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
